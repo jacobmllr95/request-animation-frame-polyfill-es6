59c2ef94f039989f7dc42a93f5fe8bc69d94d30e
Jacob Müller <jacob.mueller.elz@gmail.com>
2019-06-20 21:36:30 +0200

    Fix detection logic

--- src/index.js
+++ src/index.js
@@ -1,34 +1,27 @@
-/*!
-* requestAnimationFrame Polyfill
-*
-* @version 0.1.0
-* @overview A polyfill for requestAnimationFrame written in ES6.
-* @author Jacob Müller [jacob.mueller.elz@gmail.com]
-* @see {@link https://github.com/jackmu95/request-animation-frame-polyfill-es6|GitHub}
-*/
-
 (window => {
   if (window.requestAnimationFrame) {
     return;
   }
 
-  let lastTime = 0;
-  const vendors = ['moz', 'ms', 'o', 'webkit'];
-  for (let i = 0; i < vendors.length; i++) {
-    window.requestAnimationFrame =
-        window[`${vendors[i]}RequestAnimationFrame`];
+  ['moz', 'ms', 'o', 'webkit'].forEach(vendor => {
+    window.requestAnimationFrame = window[`${vendor}RequestAnimationFrame`];
     window.cancelAnimationFrame =
-        window[`${vendors[i]}CancelAnimationFrame`] ||
-        window[`${vendors[i]}CancelRequestAnimationFrame`];
+      window[`${vendor}CancelAnimationFrame`] || window[`${vendor}CancelRequestAnimationFrame`];
+  });
+
+  if (!window.requestAnimationFrame) {
+    let lastTime = 0;
+
+    window.requestAnimationFrame = callback => {
+      const now = new Date().getTime();
+      const timeToCall = Math.max(0, 16 - (now - lastTime));
+      return window.setTimeout(() => {
+        callback((lastTime = now + timeToCall));
+      }, timeToCall);
+    };
   }
 
-  window.requestAnimationFrame = (callback, element) => {
-    const now = new Date().getTime();
-    const timeToCall = Math.max(0, 16 - (now - lastTime));
-    return window.setTimeout(() => {
-      callback(lastTime = now + timeToCall);
-    }, timeToCall);
-  };
-
-  window.cancelAnimationFrame = window.clearTimeout;
+  if (!window.cancelAnimationFrame) {
+    window.cancelAnimationFrame = window.clearTimeout;
+  }
 })(window);
